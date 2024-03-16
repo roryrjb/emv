702f05c53534b2b05d60e2da0372bca1c022ad35
Rory Bradford <roryrjb@gmail.com>
2024-03-16 14:56:30 +0000

    Initial commit

---
+++ emv/emv.py
@@ -0,0 +1,44 @@
+import os
+import tempfile
+import subprocess
+import sys
+
+
+def main():
+    files = sys.argv[1:]
+    
+    with tempfile.NamedTemporaryFile(mode="w+t", delete=False) as tmpfile:
+        filenames_file = tmpfile.name
+
+        if files:
+            src = files
+        else:
+            src = [f for f in os.listdir(".") if os.path.isfile(f)]
+        tmpfile.write("\n".join(src))
+
+    editor = os.environ.get("EDITOR", "vim").split(" ")
+    subprocess.call(editor + [filenames_file])
+
+    with open(filenames_file, "r") as tmpfile:
+        dest = tmpfile.read().splitlines()
+
+    os.remove(filenames_file)
+
+    if len(src) != len(dest):
+        print(
+            "WARN: Number of files changed. Did you delete a line by accident? Aborting..",
+            file=sys.stderr,
+        )
+        sys.exit(1)
+
+    count = 0
+    for s, d in zip(src, dest):
+        if s != d:
+            dd = os.path.dirname(d)
+
+            if dd:
+                os.makedirs(dd, exist_ok=True)
+            os.rename(s, d)
+            count += 1
+
+    print(f"{count} files renamed.")
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="emv",
+    version="0.1",
+    description="Rename files with your editor",
+    url="https://github.com/roryrjb/emv",
+    author="Rory Bradford",
+    author_email="roryrjb@gmail.com",
+    license="MIT",
+    install_requires=[],
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "emv=emv.emv:main",
+        ],
+    },
+    zip_safe=False,
+)
